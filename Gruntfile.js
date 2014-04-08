f4d49a711b1827bd861a26572b94be5e66364b84
Billy Bastardi <billy@yext.com>
2014-04-07 23:34:19 -0400

    fix gruntfile syntax

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,51 @@
 module.exports = function( grunt ) {
 
-	'use strict';
+  'use strict';
 
-	var distpaths = [
-			'dist/feperf.js',
-			'dist/feperf.min.map',
-			'dist/feperf.min.js'
-		],
-		readOptionalJSON = function( filepath ) {
-			var data = {};
-			try {
-				data = grunt.file.readJSON( filepath );
-			} catch(e) {}
-			return data;
-		};
+  var distpaths = [
+      'dist/feperf.js',
+      'dist/feperf.min.map',
+      'dist/feperf.min.js'
+    ],
+    readOptionalJSON = function( filepath ) {
+      var data = {};
+      try {
+        data = grunt.file.readJSON( filepath );
+      } catch(e) {}
+      return data;
+    };
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		concat: {
-			dist:{
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist:{
         src: [
           'src/js/wab.js',
           'src/js/debug.js',
           'src/js/debug/profiler.js',
           'src/js/debug/toolbar.js',
         ],
-				dest: 'dist/feperf.js'
-			}
-		},
+        dest: 'dist/feperf.js'
+      }
+    },
 
-		uglify: {
+    uglify: {
       build: {
         src: 'dist/feperf.js',
         dest: 'dist/feperf.min.js'
-			},
-			options: {
-				compress: {
-					hoist_funs: false,
-					join_vars: false,
-					loops: false,
-					unused: false
-				},
-				beautify: {
-					ascii_only: true
-				}
-			}
-		},
+      },
+      options: {
+        compress: {
+          hoist_funs: false,
+          join_vars: false,
+          loops: false,
+          unused: false
+        },
+        beautify: {
+          ascii_only: true
+        }
+      }
+    },
 
     copy: {
       main: {
@@ -65,12 +65,12 @@ module.exports = function( grunt ) {
         ]
       }
     }
-	});
+  });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
-	// Default grunt
-	grunt.registerTask('default', ['concat', 'uglify', 'copy']);
+  // Default grunt
+  grunt.registerTask('default', ['concat', 'uglify', 'copy']);
 };
